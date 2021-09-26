5b74272d300cb1a952fd299fa14b097ab24e2448
CousinJeffrey <cousinjeffrey@protonmail.com>
2021-09-26 10:24:39 -0700

    Add nouns.wtf url, add logging function so the date is printed

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ import { config, getContract, getImage, getSupply } from './config.js';
 import { Client, Intents, MessageEmbed, MessageAttachment } from 'discord.js';
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 
-var current_supply = 51;
+var current_supply = 54;
+
+function log(text) {
+  console.log(`${new Date().toISOString()}\t${text}`);
+}
 
 async function checkSupply() {
   const new_current_supply = await getSupply();
   if (current_supply == 0) {
     current_supply = new_current_supply;
-    console.log(`current_supply: ${current_supply}`);
+    log(`current_supply: ${current_supply}`);
   } else if (new_current_supply > current_supply) {
     for (let i = current_supply; i < new_current_supply; i++) {
       const png = await getImage(i);
       const attachmentName = `Auction-${i}.png`;
       const attachment = new MessageAttachment(png, attachmentName);
-      console.log(`Noun ${i}`);
+      log(`Noun ${i} sent to Discord`);
       current_supply = new_current_supply;
       const message = new MessageEmbed()
         .setTitle(`*** Noun ${i} is live! ***`)
@@ -23,6 +27,7 @@ async function checkSupply() {
         .addField('Voting guide', "🔥 (it's amazing)\n\n🪙 (if it's value)\n\n✅ (it's good looking)\n\n♻️ (pass, even if it's cheap)", false)
         .addField('Got any comments?', "Chat in the thread below!", false)
         .setImage(`attachment://${attachmentName}`)
+        .setURL('https://nouns.wtf')
         .setTimestamp();
       await client.channels.cache.get(config.discord_channel).send({ embeds: [message], files:[attachment] }).then(m => {
         m.react('🔥');
@@ -32,7 +37,6 @@ async function checkSupply() {
         m.startThread({
           name: `Noun ${i}`,
           autoArchiveDuration: 60*24,
-          reason: 'Talk amongst yourselves',
         });
       });
     }
@@ -42,5 +46,9 @@ async function checkSupply() {
 const seconds = 10, the_interval = seconds * 1000;
 setInterval(function() { checkSupply();}, the_interval);
 
-await getContract();
+try {
+  await getContract();
+} catch (e) {
+  console.error("Error getting contract");
+}
 client.login(config.DISCORD_KEY);
